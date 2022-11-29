3c276d1c1354aa8cf8ac6745e99dc6927d304b20
Kevin0322 <54361431+Kevin0322@users.noreply.github.com>
2022-11-29 08:56:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log('Hello world');
+let interestRate = 2;
+let loan = 5
+console.log(interestRate);
+console.log(loan);
+
+let totalRepay = interestRate*loan
+console.log(totalRepay)
+
+
+let person = {name: 'Josh',Age:25,
+};
+console.log(person);
+
